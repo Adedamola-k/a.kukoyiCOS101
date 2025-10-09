b1d2384bf0c63726efff33f171a14e123a23b97d
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-09 21:46:33 +0100

    (depreciation)

---
+++ week-2/project_3.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+	let p:f64 = 510000.0;
+	let r:f64 = 5.0;
+	let n:f64 = 3.0;
+
+	//calculate amount
+	let a = p * (1.0 - (r / 100.0)).powf(n);
+
+	//calculate compound interest 
+	let ci = p - a;
+
+	//print depreciation
+	println!("depreciation is {}", ci );
+}
