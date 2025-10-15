729f9b9afdb29d79db1f695c03deafb6082e95aa
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-15 22:29:55 +0100

    (surname middle and first name)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+   let special_character = '@' ;  //default
+   let alphabet:char = 'b';
+   let surname = "bolaji";
+   let first_name = "micheal";
+   let middle_name = "gboyega";
+
+   println!(" ");
+   println!("special character: {}",special_character);
+   println!("alphabet: {}",alphabet);
+   println!("name: {} {} {}",surname, middle_name, first_name);
+}
