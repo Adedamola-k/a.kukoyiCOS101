ba8e1b91a45b411e0dbf292ed7e5c28e615d7f81
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-16 01:35:48 +0100

    (calculations)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //addition
+    let sum = 555.0 + 731.0;
+    println!("the sum of 5550 and 7310 = {}", sum);
+
+    //subtraction 
+    let subtraction = 95.5 - 4.3;
+    println!("the difference of 95.5 and 4.3 = {}", subtraction);
+
+    //multiplication
+    let product = 4 * 30;
+    println!("the multiple of 4 and 30 = {}", product);
+
+    //division
+    let division = 56.7 / 32.2;
+    println!("the division of 56.7 and 32.2 = {}", division);
+
+    //remainder
+    let remainder = 43 % 5;
+    println!("the remainder of 43 and 5 = {}", remainder);
+}
