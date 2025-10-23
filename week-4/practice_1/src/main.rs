e8d58dcab6b81a82b15da086d875465fc3b3997c
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-23 13:18:17 +0100

    student information management system

--- week-4/practice_1/src/main.rs
+++ week-4/practice_1/src/main.rs
@@ -5,19 +5,18 @@ use std::io;
 fn main() {
 	println!("student information management system");
 
-
 	//input name
 	println!("please enter your name");
 	let mut name = String::new();
 		io::stdin()
 		.read_line(&mut name)
 		.expect("failed to read input");
-	println!("your name is: {}", name);
+	println!("your name is: {}", name );
 
-	//input age
-	println!("enter your age.");
+	//input age 
+	println!("enter your age");
 	let mut age = String::new();
 		io::stdin().read_line(&mut age).expect("failed to read input");
 	let age:i32 = age.trim().parse().expect("input not an integer");
-	println!("your age is: {}", age);
-	}
+	println!("your age is: {}", age );
+}
