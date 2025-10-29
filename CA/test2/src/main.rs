9edcc59d874f859e59f6c637b95becb5319d5bc0
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-29 13:21:49 +0100

    gross salary

---
+++ CA/test2/src/main.rs
@@ -0,0 +1,41 @@
+//input data
+use std::io;
+
+fn main() 
+{
+	let mut input = String::new();
+
+	println!("enter your employee name and hours");
+	io::stdin().read_line(&mut input).expect("not a valid string");
+	let hours:f64 = input.trim().parse().expect("not a valid number");
+
+		if time <= 40.0 
+    {
+    println!("your gross salary is {3000}", ); 
+    }
+
+	 else if hours >40
+    {
+    	println!("your gross salary is {4500}", );
+   }
+
+  	 else if gross salary >100000
+   {
+
+   	 	println!("your gross salary is {h = g - 2000}", h);
+   	}
+   	else
+   	{
+   		println!("no gross salary");
+   	}
+   
+
+    // salary 
+    let s = h + e + d 
+    println!("salary is {}", s );
+
+    //gross salary
+    let g = s/12
+    println!("gross salary {}", g );
+
+}
---
+++ CA/test2/target/debug/deps/test2.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\a.kukoyiCOS101\CA\test2\target\debug\deps\test2.d: src\main.rs
+
+C:\Users\USER\Documents\a.kukoyiCOS101\CA\test2\target\debug\deps\test2.exe: src\main.rs
+
+src\main.rs:
