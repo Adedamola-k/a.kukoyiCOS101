3af9a7a358a49dde153915ce7f24c9cedee912ff
Adedamola-k <adedamola.kukoyi@pau.edu.ng>
2025-10-29 13:23:40 +0100

    bookshop order system

---
+++ CA/test3/src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn main() 
+{
+    //display
+    let r = 15000;
+    let a = 12500;
+    let d = 20000;
+    let n = 18000;
+
+    //ask for book codes 
+    println!("enter your book code:");
+    i8::stdin().read_line(&mut input1).expect("not a valid string");
+    let mut input1 = string::new;
+    let book code:f32 = input1.trim().parse().expect("not a valid number");
+
+    //ask for book quantity
+    println!("enter your book quantity:");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("not a valid string");
+    let book quantity:i32 = input2.trim().parse().expect("not a valid number");
+
+    //total amount
+    let t = r + a + d + n;
+    println!("total amount is {}", t );
+
+    if book quantity ==<3
+    let t = (10 / 100) * y
+{
+    println!("your total amount is {}", t );
+}
+
+
+
+}
---
+++ CA/test3/target/debug/deps/test3.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\a.kukoyiCOS101\CA\test3\target\debug\deps\test3.d: src\main.rs
+
+C:\Users\USER\Documents\a.kukoyiCOS101\CA\test3\target\debug\deps\test3.exe: src\main.rs
+
+src\main.rs:
